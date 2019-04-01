f24e5638df475eb9d31c7fd15fb7da5ff831352f
Marius Meschter <mariusmeschter99@gmail.com>
2019-04-01 23:44:51 +0200

    textfit everything

--- src/components/Live/Timer/PlayerTimes.vue
+++ src/components/Live/Timer/PlayerTimes.vue
@@ -1,8 +1,9 @@
 <template>
-	<div>{{ output }}</div>
+	<div id="timer">{{ output }}</div>
 </template>
 
 <script>
+import textfit from 'textfit';
 import moment from 'moment';
 import 'moment-duration-format';
 
@@ -17,31 +18,42 @@ export default {
 	},
 	computed: {
 		output() {
+			let t = '';
 			if (this.data.currentRun.players.length === 1) {
 				return '';
-			} else if (this.$route.params.id && parseInt(this.$route.params.id, 10) >= this.data.currentRun.players.length) {
+			}
+			if (this.$route.params.id && parseInt(this.$route.params.id, 10) >= this.data.currentRun.players.length) {
 				return '';
 			}
 			const p = this.data.currentRun.players[this.$route.params.id];
 
 			// TODO: display position of player
 			if (p.timer.finished) {
-				return p.timer.time >= 3600 ? moment.duration(p.timer.time, 'seconds').format('h:mm:ss', { trim: 'false' })
+				t = p.timer.time >= 3600 ? moment.duration(p.timer.time, 'seconds').format('h:mm:ss', { trim: 'false' })
 					: moment.duration(p.timer.time, 'seconds').format('mm:ss', { trim: 'false' });
 			}
+
+			if (document.getElementsByClassName('textFitted').length > 0) {
+				document.getElementsByClassName('textFitted')[0].innerHTML = t;
+			}
+
 			return '';
 		},
+	},
+	updated() {
+		textfit(document.getElementById('timer'), {
+			alignHoriz: false,
+			alignVert: true,
+			minFontSize: 10,
+			maxFontSize: 120,
+			multiLine: false,
+		});
 	},
 };
 </script>
 
 <style scoped>
-html {
-	background-color: #00ff00;
-}
-body {
-	color: white !important;
-	font-family: Arial, Helvetica, sans-serif;
-	font-size: 50px;
+#timer {
+	height: 100vh;
 }
 </style>
